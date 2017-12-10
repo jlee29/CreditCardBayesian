1538e4fd8e96a9c2f59d49b8b63bd417be8f27c4
jlee29 <jlee29@stanford.edu>
2017-12-10 01:08:13 +0000

    added graphs

--- test.py
+++ test.py
@@ -1,11 +1,75 @@
-import os
-import numpy as np
-import pandas as pd
 from pomegranate import *
+import json
+import matplotlib.pyplot as plt
 
-labelpath = os.path.join('./data/smaller.csv')
-X = np.array(pd.read_csv(labelpath))
-X_train = X[24000]
-X_test = X[6000]
-model = BayesianNetwork.from_samples(X_train, algorithm='exact')
-print('hi')
+
+def main():
+	a = BayesianNetwork()
+
+	with open("exact0.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Exact DP Algorithm on 5 other states")
+	plt.savefig("exact5.png")
+
+	with open("exact1.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Exact DP Algorithm on 6 other states")
+	plt.savefig("exact6.png")
+
+	with open("exact2.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Exact DP Algorithm on 7 other states")
+	plt.savefig("exact7.png")
+
+	with open("exact3.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Exact DP Algorithm on 8 other states")
+	plt.savefig("exact8.png")
+
+	with open("greedy0.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Greedy Algorithm on 5 other states")
+	plt.savefig("greedy5.png")
+
+	with open("greedy1.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Greedy Algorithm on 6 other states")
+	plt.savefig("greedy6.png")
+
+	with open("greedy2.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Greedy Algorithm on 7 other states")
+	plt.savefig("greedy7.png")
+
+	with open("greedy3.json","r") as f:
+	  d = json.load(f)
+	graph = a.from_json(d)
+	plt.figure(dpi=300)
+	graph.plot()
+	plt.title("Greedy Algorithm on 8 other states")
+	plt.savefig("greedy8.png")
+
+
+if __name__ == '__main__':
+	main()
